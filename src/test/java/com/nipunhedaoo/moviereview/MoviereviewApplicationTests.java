81c2b110ee8484c310bb79454f56b8d4dcf53d7e
Nipun Hedaoo <nipunhedaoo@gmail.com>
2023-06-05 14:07:46 -0400

    Initialize project

---
+++ src/test/java/com/nipunhedaoo/moviereview/MoviereviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nipunhedaoo.moviereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviereviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

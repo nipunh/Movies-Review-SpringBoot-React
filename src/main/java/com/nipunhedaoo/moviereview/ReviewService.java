421b425ac2964eeeb066715c9a00752a1acc1fa2
Nipun Hedaoo <nipunhedaoo@gmail.com>
2023-06-07 10:41:08 -0400

    create review

--- src/main/java/com/nipunhedaoo/moviereview/ReviewService.java
+++ src/main/java/com/nipunhedaoo/moviereview/ReviewService.java
@@ -18,8 +18,7 @@ public class ReviewService {
 
         mongoTemplate.update(Movie.class).matching(Criteria.where("imdbId").is(imdbId))
                 .apply(new Update().push("reviewIds").value(review)).first();
-
-
+        
         return review;
     }
 }
